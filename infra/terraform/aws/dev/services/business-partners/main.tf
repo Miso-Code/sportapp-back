15408022f146bc9f652713bf93d6f5df04ec00b8
Brayan Henao <b.henaoc@uniandes.edu.co>
2024-04-17 01:59:51 -0500

    feat: PSA-185 PSA-186 Add create and get all business_partners services/products terraform code

---
+++ infra/terraform/aws/dev/services/business-partners/main.tf
@@ -0,0 +1,132 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_iam_role" "ecs_role" {
+  name = "ecsTaskExecutionRole"
+}
+
+data "aws_secretsmanager_secret" "db_credentials" {
+  name = "DB_CREDENTIALS_DEV"
+}
+
+data "aws_secretsmanager_secret" "jwt_secret" {
+  name = "JWT_SECRET"
+}
+
+data "terraform_remote_state" "resources" {
+  backend = "remote"
+  config = {
+    organization = "MisoTeam"
+    workspaces = {
+      name = "aws-resources-dev"
+    }
+  }
+}
+
+// Register target group and listener rule
+module "business-partners-tg" {
+  source                         = "../../../modules/elb/target_group"
+  target_group_name              = "business-partners-dev-tg"
+  target_group_port              = 8000
+  target_group_protocol          = "HTTP"
+  target_group_health_check_path = "/ping"
+  vpc_id                         = data.terraform_remote_state.resources.outputs.vpc_id
+}
+
+module "business-partners-listener-rule" {
+  source                = "../../../modules/elb/listener_rule"
+  listener_arn          = data.terraform_remote_state.resources.outputs.elb_listener_arn
+  rule_path_pattern     = "/business-partners/*"
+  rule_priority         = 1
+  rule_target_group_arn = module.business-partners-tg.tg_arn
+}
+
+// Register task definition
+module "business-partners-task-def" {
+  source                = "../../../modules/ecs/task_definition"
+  service_name          = "business-partners-dev"
+  container_image       = "887664210442.dkr.ecr.us-east-1.amazonaws.com/business-partners:develop"
+  container_port        = 8000
+  cpu                   = 256
+  memory                = 512
+  task_role_arn         = data.aws_iam_role.ecs_role.arn
+  execution_role_arn    = data.aws_iam_role.ecs_role.arn
+  environment_variables = []
+  secrets               = [
+    {
+      "valueFrom" : "${data.aws_secretsmanager_secret.db_credentials.arn}:NAME::"
+      "name" : "DB_NAME"
+    },
+    {
+      "valueFrom" : "${data.aws_secretsmanager_secret.db_credentials.arn}:HOST::"
+      "name" : "DB_HOST"
+    },
+    {
+      "valueFrom" : "${data.aws_secretsmanager_secret.db_credentials.arn}:PORT::"
+      "name" : "DB_PORT"
+    },
+    {
+      "valueFrom" : "${data.aws_secretsmanager_secret.db_credentials.arn}:USERNAME::"
+      "name" : "DB_USERNAME"
+    },
+    {
+      "valueFrom" : "${data.aws_secretsmanager_secret.db_credentials.arn}:PASSWORD::"
+      "name" : "DB_PASSWORD"
+    },
+    {
+      "valueFrom" : "${data.aws_secretsmanager_secret.jwt_secret.arn}:JWT_SECRET::"
+      "name" : "JWT_SECRET_KEY"
+    }
+  ]
+}
+
+// Register service
+module "business-partners-service" {
+  source              = "../../../modules/ecs/service"
+  service_name        = "business-partners-dev-service"
+  desired_count       = 1
+  container_port      = 8000
+  cluster_id          = data.terraform_remote_state.resources.outputs.ecs_cluster_id
+  security_groups     = data.terraform_remote_state.resources.outputs.security_groups
+  subnets             = data.terraform_remote_state.resources.outputs.subnets
+  target_group_arn    = module.business-partners-tg.tg_arn
+  task_definition_arn = module.business-partners-task-def.task_arn
+}
+
+
+module "business-partners-registration-route" {
+  source           = "../../../modules/api_gateway/route"
+  api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
+  route_method     = "POST"
+  route_path       = "/business-partners/registration"
+  elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
+  vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
+}
+
+module "business-partners-login-route" {
+  source           = "../../../modules/api_gateway/route"
+  api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
+  route_method     = "POST"
+  route_path       = "/business-partners/login"
+  elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
+  vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
+}
+
+module "business-partners-create-product-route" {
+  source           = "../../../modules/api_gateway/route"
+  api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
+  route_method     = "POST"
+  route_path       = "/business-partners/products"
+  elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
+  vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
+}
+
+module "business-partners-get-all-products-route" {
+  source           = "../../../modules/api_gateway/route"
+  api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
+  route_method     = "GET"
+  route_path       = "/business-partners/products"
+  elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
+  vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
+}
