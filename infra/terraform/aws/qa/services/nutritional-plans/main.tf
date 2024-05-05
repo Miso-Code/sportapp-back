3aeafd8e29b6150aca8dfc6b1dbdbbc57fc5f2b8
Brayan Henao <b.henaoc@uniandes.edu.co>
2024-05-05 04:12:36 -0500

    feat: PSA-208 PSA-212 CalorIC intake notifications and nutritional plan terraform code added

--- infra/terraform/aws/qa/services/nutritional-plans/main.tf
+++ infra/terraform/aws/qa/services/nutritional-plans/main.tf
@@ -7,11 +7,7 @@ data "aws_iam_role" "ecs_role" {
 }
 
 data "aws_secretsmanager_secret" "db_credentials" {
-  name = "DB_CREDENTIALS_PROD"
-}
-
-data "aws_secretsmanager_secret" "firebase_credentials" {
-  name = "GCLOUD_CREDENTIALS"
+  name = "DB_CREDENTIALS_QA"
 }
 
 
@@ -20,44 +16,40 @@ data "terraform_remote_state" "resources" {
   config = {
     organization = "MisoTeam"
     workspaces = {
-      name = "aws-resources-prod"
+      name = "aws-resources-qa"
     }
   }
 }
 
 // Register target group and listener rule
-module "alerts-tg" {
+module "nutritional-plans-tg" {
   source                         = "../../../modules/elb/target_group"
-  target_group_name              = "alerts-tg"
+  target_group_name              = "nutritional-plans-qa-tg"
   target_group_port              = 8000
   target_group_protocol          = "HTTP"
   target_group_health_check_path = "/ping"
   vpc_id                         = data.terraform_remote_state.resources.outputs.vpc_id
 }
 
-module "alerts-listener-rule" {
+module "nutritional-plans-listener-rule" {
   source                = "../../../modules/elb/listener_rule"
   listener_arn          = data.terraform_remote_state.resources.outputs.elb_listener_arn
-  rule_path_pattern     = "/alerts/*"
-  rule_priority         = 8
-  rule_target_group_arn = module.alerts-tg.tg_arn
+  rule_path_pattern     = "/nutritional-plans/*"
+  rule_priority         = 9
+  rule_target_group_arn = module.nutritional-plans-tg.tg_arn
 }
 
 // Register task definition
-module "alerts-task-def" {
+module "nutritional-plans-task-def" {
   source                = "../../../modules/ecs/task_definition"
-  service_name          = "alerts"
-  container_image       = "887664210442.dkr.ecr.us-east-1.amazonaws.com/alerts:latest"
+  service_name          = "nutritional-plans-qa"
+  container_image       = "887664210442.dkr.ecr.us-east-1.amazonaws.com/nutritional-plans:develop"
   container_port        = 8000
   cpu                   = 256
   memory                = 512
   task_role_arn         = data.aws_iam_role.ecs_role.arn
   execution_role_arn    = data.aws_iam_role.ecs_role.arn
   environment_variables = [
-    {
-      "name" : "ADVERSE_INCIDENTS_ALERTS_QUEUE",
-      "value" : data.terraform_remote_state.resources.outputs.adverse_incidents_queue_name
-    },
     {
       "name" : "NUTRITIONAL_PLAN_ALERTS_QUEUE",
       "value" : data.terraform_remote_state.resources.outputs.nutritional_queue_name
@@ -84,50 +76,48 @@ module "alerts-task-def" {
       "valueFrom" : "${data.aws_secretsmanager_secret.db_credentials.arn}:PASSWORD::"
       "name" : "DB_PASSWORD"
     },
-    {
-      "valueFrom" : "${data.aws_secretsmanager_secret.firebase_credentials.arn}:GCLOUD_CREDENTIALS::"
-      "name" : "FIREBASE_CERT"
-    }
   ]
 }
 
 // Register service
-module "alerts-service" {
+module "nutritional-plans-service" {
   source              = "../../../modules/ecs/service"
-  service_name        = "alerts-service"
+  service_name        = "nutritional-plans-qa-service"
   desired_count       = 1
   container_port      = 8000
   cluster_id          = data.terraform_remote_state.resources.outputs.ecs_cluster_id
   security_groups     = data.terraform_remote_state.resources.outputs.security_groups
   subnets             = data.terraform_remote_state.resources.outputs.subnets
-  target_group_arn    = module.alerts-tg.tg_arn
-  task_definition_arn = module.alerts-task-def.task_arn
+  target_group_arn    = module.nutritional-plans-tg.tg_arn
+  task_definition_arn = module.nutritional-plans-task-def.task_arn
 }
 
 
-module "alerts-register-device-route" {
+module "nutritional-plans-create-plan" {
   source           = "../../../modules/api_gateway/route"
   api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
   route_method     = "POST"
-  route_path       = "/alerts/register-device"
+  route_path       = "/nutritional-plans"
+  special_path     = true
   elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
   vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
 }
 
-module "alerts-disable-device-route" {
-  source           = "../../../modules/api_gateway/route"
-  api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
-  route_method     = "POST"
-  route_path       = "/alerts/disable-device"
-  elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
-  vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
-}
-
-module "alerts-get-device-route" {
+module "nutritional-plans-get-plan" {
   source           = "../../../modules/api_gateway/route"
   api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
   route_method     = "GET"
-  route_path       = "/alerts/device"
+  route_path       = "/nutritional-plans"
+  special_path     = true
+  elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
+  vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
+}
+
+module "nutritional-plans-notify-caloric-intake" {
+  source           = "../../../modules/api_gateway/route"
+  api_id           = data.terraform_remote_state.resources.outputs.api_gateway_id
+  route_method     = "POST"
+  route_path       = "/nutritional-plans/notify-caloric-intake"
   elb_listener_arn = data.terraform_remote_state.resources.outputs.elb_listener_arn
   vpc_link_id      = data.terraform_remote_state.resources.outputs.vpc_link_id
 }
