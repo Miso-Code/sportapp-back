c007307ef3280d4817cf8846fe3b602c4afdd788
Brayan Henao <b.henaoc@uniandes.edu.co>
2024-05-11 01:35:29 -0500

    fix: Fix error with env in adverse-incidents-provider

--- projects/adverse-incidents-provider/app/config/settings.py
+++ projects/adverse-incidents-provider/app/config/settings.py
@@ -4,8 +4,8 @@ from app.models.schemas.schema import Coordinate
 
 
 class Config:
-    MAX_ADVERSE_INCIDENTS = os.getenv("MAX_ADVERSE_INCIDENTS", 5)
-    ADVERSE_INCIDENTS_AFFECTED_RANGE = os.getenv("ADVERSE_INCIDENTS_AFFECTED_RANGE", 0.5)
+    MAX_ADVERSE_INCIDENTS = int(os.getenv("MAX_ADVERSE_INCIDENTS", 5))
+    ADVERSE_INCIDENTS_AFFECTED_RANGE = float(os.getenv("ADVERSE_INCIDENTS_AFFECTED_RANGE", 0.5))
     INCIDENTS_API_KEY = os.getenv("INCIDENTS_API_KEY", "secret")
     CALI_BOUNDARY_CODES = [
         Coordinate(latitude=-76.8635495958, longitude=3.1194990575),
